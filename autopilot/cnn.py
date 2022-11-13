db601614702734a7c700514cabd0f041b0c9cc3f
akilhylton <hyltonakil@gmail.com>
2022-11-12 21:42:09 -0500

    Formatted code

--- autopilot/cnn.py
+++ autopilot/cnn.py
@@ -8,35 +8,43 @@ def getModel(img_width, img_height, img_channels, output_dim):
     m = dronet(img_width, img_height, img_channels, output_dim)
     return m
 
+
 def train(train_data, val_data, model, initial_epoch):
     # Initialize loss weights
-    model.alpha = tf.Variable(1, trainable=False, name='alpha', dtype=tf.float32)
-    model.beta = tf.Variable(0, trainable=False, name='beta', dtype=tf.float32)
+    model.alpha = tf.Variable(1, trainable=False, name="alpha", dtype=tf.float32)
+    model.beta = tf.Variable(0, trainable=False, name="beta", dtype=tf.float32)
 
     # Initialize number of samples for hard-mining
-    model.k_mse = tf.Variable(32, trainable=False, name='k_mse', dtype=tf.int32)
-    model.k_entropy = tf.Variable(32, trainable=False, name='k_entropy', dtype=tf.int32)
+    model.k_mse = tf.Variable(32, trainable=False, name="k_mse", dtype=tf.int32)
+    model.k_entropy = tf.Variable(32, trainable=False, name="k_entropy", dtype=tf.int32)
 
     lr_schedule = tf.keras.optimizers.schedules.ExponentialDecay(
-        initial_learning_rate=1e-2,
-        decay_steps=10000,
-        decay_rate=1e-5)
+        initial_learning_rate=1e-2, decay_steps=10000, decay_rate=1e-5
+    )
 
     optimizer = tf.keras.optimizers.Adam(learning_rate=lr_schedule)
     # Configure training process
-    # TODD("Add training process config")    
-    model.compile(optimizer=optimizer,
-                  loss=[utils.hard_mining_mse(model.k_mse),
-                        utils.hard_mining_entropy(model.k_entropy)],
-                    loss_weights=[model.alpha, model.beta]
+    # TODD("Add training process config")
+    model.compile(
+        optimizer=optimizer,
+        loss=[
+            utils.hard_mining_mse(model.k_mse),
+            utils.hard_mining_entropy(model.k_entropy),
+        ],
+        loss_weights=[model.alpha, model.beta],
     )
     # Train model
     steps_per_epoch = int(np.ceil(train_data.samples / 32))
-    val_steps = int(np.ceil(val_data.samples /32 ))
+    val_steps = int(np.ceil(val_data.samples / 32))
 
-    model.fit_generator(train_data, epochs = 1, 
-    steps_per_epoch = steps_per_epoch, validation_data = val_data, 
-    validation_steps = val_steps, initial_epoch = initial_epoch)
+    model.fit_generator(
+        train_data,
+        epochs=1,
+        steps_per_epoch=steps_per_epoch,
+        validation_data=val_data,
+        validation_steps=val_steps,
+        initial_epoch=initial_epoch,
+    )
 
     # Save the model in tflite format
     # Convert the model.
@@ -44,40 +52,46 @@ def train(train_data, val_data, model, initial_epoch):
     tflite_model = converter.convert()
 
     # Save the model.
-    with open('model.tflite', 'wb') as f:
+    with open("model.tflite", "wb") as f:
         f.write(tflite_model)
 
 
-output_dim = 1
-img_mode = "grayscale"
-img_width = 320
-img_height = 240
-crop_img_height, crop_img_width = 200, 200
+def main():
+    output_dim = 1
+    img_mode = "grayscale"
+    img_width = 320
+    img_height = 240
 
-# Generate training data with real-time augmentation
-train_datagen = tf.keras.preprocessing.image.ImageDataGenerator(
-                                            rotation_range = 0.2,
-                                             rescale = 1./255,
-                                             width_shift_range = 0.2,
-                                             height_shift_range=0.2
-)
+    # Generate training data with real-time augmentation
+    train_datagen = tf.keras.preprocessing.image.ImageDataGenerator(
+        rotation_range=0.2,
+        rescale=1.0 / 255,
+        width_shift_range=0.2,
+        height_shift_range=0.2,
+    )
 
-train_generator = train_datagen.flow_from_directory("../collision_dataset/training", 
-                                                                            shuffle = True, 
-                                                                            color_mode=img_mode, 
-                                                                            target_size=(img_width, img_height), 
-                                                                            batch_size =32)
+    train_generator = train_datagen.flow_from_directory(
+        "../training",
+        shuffle=True,
+        color_mode=img_mode,
+        target_size=(img_width, img_height),
+        batch_size=32,
+    )
 
-                                                                         
+    val_datagen = tf.keras.preprocessing.image.ImageDataGenerator(rescale=1.0 / 255)
 
-val_datagen = tf.keras.preprocessing.image.ImageDataGenerator(rescale = 1./255)
+    val_generator = val_datagen.flow_from_directory(
+        "../validation",
+        shuffle=True,
+        color_mode=img_mode,
+        target_size=(img_width, img_height),
+        batch_size=32,
+    )
 
-val_generator = val_datagen.flow_from_directory("../collision_dataset/validation",
-                                                                            shuffle = True, 
-                                                                            color_mode=img_mode, 
-                                                                            target_size=(img_width, img_height), 
-                                                                            batch_size =32)
+    model = getModel(img_width, img_height, 1, output_dim)
 
-model = getModel(img_width,img_height, 1, output_dim)
+    train(train_generator, val_generator, model, 0)
 
-train(train_generator, val_generator, model, 0)
+
+if __name__ == "__main__":
+    main()
--- autopilot/utils.py
+++ autopilot/utils.py
@@ -13,10 +13,10 @@ def hard_mining_mse(k):
 
     def custom_mse(y_true, y_pred):
         # Parameter t indicates the type of experiment
-        t = y_true[:,0]
+        t = y_true[:, 0]
 
         # Number of steering samples
-        samples_steer = tf.cast(tf.equal(t,1), tf.int32)
+        samples_steer = tf.cast(tf.equal(t, 1), tf.int32)
         n_samples_steer = tf.reduce_sum(samples_steer)
 
         if n_samples_steer == 0:
@@ -24,7 +24,7 @@ def hard_mining_mse(k):
         else:
             # Predicted and real steerings
             pred_steer = tf.squeeze(y_pred, axis=-1)
-            true_steer = y_true[:,1]
+            true_steer = y_true[:, 1]
 
             # Steering loss
             l_steer = tf.multiply(t, K.square(pred_steer - true_steer))
@@ -33,12 +33,11 @@ def hard_mining_mse(k):
             k_min = tf.minimum(k, n_samples_steer)
             _, indices = tf.nn.top_k(l_steer, k=k_min)
             max_l_steer = tf.gather(l_steer, indices)
-            hard_l_steer = tf.divide(tf.reduce_sum(max_l_steer), tf.cast(k,tf.float32))
+            hard_l_steer = tf.divide(tf.reduce_sum(max_l_steer), tf.cast(k, tf.float32))
 
             return hard_l_steer
 
     return custom_mse
-
 
 
 def hard_mining_entropy(k):
@@ -52,10 +51,10 @@ def hard_mining_entropy(k):
 
     def custom_bin_crossentropy(y_true, y_pred):
         # Parameter t indicates the type of experiment
-        t = y_true[:,0]
+        t = y_true[:, 0]
 
         # Number of collision samples
-        samples_coll = tf.cast(tf.equal(t,0), tf.int32)
+        samples_coll = tf.cast(tf.equal(t, 0), tf.int32)
         n_samples_coll = tf.reduce_sum(samples_coll)
 
         if n_samples_coll == 0:
@@ -63,10 +62,10 @@ def hard_mining_entropy(k):
         else:
             # Predicted and real labels
             pred_coll = tf.squeeze(y_pred, axis=-1)
-            true_coll = y_true[:,1]
+            true_coll = y_true[:, 1]
 
             # Collision loss
-            l_coll = tf.multiply((1-t), K.binary_crossentropy(true_coll, pred_coll))
+            l_coll = tf.multiply((1 - t), K.binary_crossentropy(true_coll, pred_coll))
 
             # Hard mining
             k_min = tf.minimum(k, n_samples_coll)
