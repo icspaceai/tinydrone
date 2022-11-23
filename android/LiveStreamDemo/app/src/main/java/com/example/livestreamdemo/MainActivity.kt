b1fca7e035ce3927005d65479771fb5423b5ad42
Noah Camara <noahcmr@gmail.com>
2022-11-23 10:45:22 -0500

    Added a record button. No functionality yet.

--- android/LiveStreamDemo/app/src/main/java/com/example/livestreamdemo/MainActivity.kt
+++ android/LiveStreamDemo/app/src/main/java/com/example/livestreamdemo/MainActivity.kt
@@ -4,11 +4,17 @@ import android.media.ImageReader
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.absoluteOffset
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import com.example.livestreamdemo.screens.CameraPreviewScreen
+import com.example.livestreamdemo.screens.recordButton
 import com.example.livestreamdemo.ui.theme.LiveStreamDemoTheme
 
 class MainActivity : ComponentActivity() {
@@ -23,6 +29,7 @@ class MainActivity : ComponentActivity() {
                 ) {
                     CameraPreviewScreen()
                 }
+                    recordButton()
             }
         }
     }
--- android/LiveStreamDemo/app/src/main/java/com/example/livestreamdemo/screens/CameraPreviewScreen.kt
+++ android/LiveStreamDemo/app/src/main/java/com/example/livestreamdemo/screens/CameraPreviewScreen.kt
@@ -8,11 +8,21 @@ import androidx.camera.core.CameraSelector
 import androidx.camera.core.Preview
 import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.camera.view.PreviewView
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Button
+import androidx.compose.material.ButtonDefaults
+import androidx.compose.material.Icon
+import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalLifecycleOwner
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.core.content.ContextCompat
 import com.example.livestreamdemo.util.Common.REQUIRED_PERMISSIONS
@@ -22,7 +32,6 @@ fun CameraPreviewScreen() {
     val lifecycleOwner = LocalLifecycleOwner.current
     val context = LocalContext.current
     val cameraProviderNext = remember { ProcessCameraProvider.getInstance(context) }
-
 
 
     var hasCamPermission by remember {
@@ -36,7 +45,7 @@ fun CameraPreviewScreen() {
     val launcher = rememberLauncherForActivityResult(
         contract = ActivityResultContracts.RequestMultiplePermissions(),
         onResult = { granted ->
-            hasCamPermission =granted.size == 2
+            hasCamPermission = granted.size == 2
         }
     )
 
@@ -48,7 +57,7 @@ fun CameraPreviewScreen() {
             )
         )
     }
-    if(hasCamPermission) {
+    if (hasCamPermission) {
         AndroidView(
             factory = { ctx ->
                 val previewView = PreviewView(ctx)
@@ -77,5 +86,30 @@ fun CameraPreviewScreen() {
             },
             modifier = Modifier.fillMaxSize(),
         )
+    }
+}
+
+@Composable
+fun recordButton() {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .background(color = Color.Transparent)
+            .offset(y = (600).dp)
+    ) {
+        Button(
+            onClick = { /* record method */ },
+            contentPadding = PaddingValues(
+                start = 20.dp,
+                top = 12.dp,
+                end = 20.dp,
+                bottom = 12.dp
+            ),
+            modifier = Modifier
+                .align(Alignment.CenterHorizontally)
+        ) {
+            Spacer(Modifier.size(ButtonDefaults.IconSpacing))
+            Text(text = "Record")
+        }
     }
 }
